2626cb3898cf46059b72434d4f8e034c5aa09e5e
ChristianY1 <christianyunga@gmail.com>
2020-06-15 21:24:58 -0500

    clase

---
+++ src/ec/edu/ups/Modelo/ComprobanteDeVenta.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.Modelo;
+
+import java.sql.Date;
+
+import javax.xml.crypto.Data;
+
+public class ComprobanteDeVenta {
+	private String tipo;
+	private Date fecha;
+	private int numero;
+	
+	
+}
