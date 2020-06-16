f41e6b1d27138042a8de5bd79b645e726b561be2
ChristianY1 <christianyunga@gmail.com>
2020-06-15 21:43:33 -0500

    clase

--- src/ec/edu/ups/Modelo/ComprobanteDeVenta.java
+++ src/ec/edu/ups/Modelo/ComprobanteDeVenta.java
@@ -8,6 +8,24 @@ public class ComprobanteDeVenta {
 	private String tipo;
 	private Date fecha;
 	private int numero;
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
 	
 	
 }
