59219c7736896e2618cc0aabacf7752c383431fd
pablocalle1998 <prosebascalle@hotmail.com>
2020-06-19 10:15:31 -0500

    Creacion de controladores

---
+++ src/ec/edu/ups/Controlador/ControladorFactutaDetalle.java
@@ -0,0 +1,37 @@
+package ec.edu.ups.Controlador;
+
+import java.util.List;
+
+
+public class ControladorFactutaDetalle {
+	
+	private Caja caja;
+	
+	
+	
+	
+	public double crearTransaccionSueldo(double p1, String p2, int p3) {
+		return 0;
+	}
+	
+	public double calcularDebe(double ganancia) {
+		return 0;
+	}
+	
+	public double haber(double deudas) {
+		return 0;
+	}
+	
+	public boolean guardarTransaccion() {
+		return false;
+	}
+	
+	public List<ComprobanteDeVenta> registrarComprobante(ComprobanteDeVenta cdv) {
+		return null;
+	}
+	
+	public double actualizarSaldo(double debe, double haber) {
+		return 0;
+	}
+
+}
