5348a15df8413f09a61c4381345df32b613d68f6
ChristianY1 <christianyunga@gmail.com>
2020-06-19 23:14:56 -0500

    cambios

--- src/ec/edu/ups/Modelo/HistoriasClinicas.java
+++ src/ec/edu/ups/Modelo/HistoriasClinicas.java
@@ -13,45 +13,7 @@ public class HistoriasClinicas {
 		super();
 	}
 	
-	public HistoriasClinicas(int historia_id, String fecha, String historia, String observaciones) {
-		super();
-		this.historia_id = historia_id;
-		this.fecha = fecha;
-		this.historia = historia;
-		this.observaciones = observaciones;
-	}
-
-	public int getHistoria_id() {
-		return historia_id;
-	}
-
-	public void setHistoria_id(int historia_id) {
-		this.historia_id = historia_id;
-	}
-
-	public String getFecha() {
-		return fecha;
-	}
-
-	public void setFecha(String fecha) {
-		this.fecha = fecha;
-	}
-
-	public String getHistoria() {
-		return historia;
-	}
-
-	public void setHistoria(String historia) {
-		this.historia = historia;
-	}
-
-	public String getObservaciones() {
-		return observaciones;
-	}
-
-	public void setObservaciones(String observaciones) {
-		this.observaciones = observaciones;
-	}
+	
 
 	@Override
 	public String toString() {
--- src/ec/edu/ups/Modelo/Sueldo.java
+++ src/ec/edu/ups/Modelo/Sueldo.java
@@ -10,25 +10,7 @@ public abstract class Sueldo extends CuentaContable{
 	public Sueldo() {
 		super();
 	}
-	public Sueldo(Caja caja, double cantidad) {
-		super();
-		this.caja = caja;
-		this.cantidad = cantidad;
-	}
-	public Caja getCaja() {
-		return caja;
-	}
 	
-	
-	public void setCaja(Caja caja) {
-		this.caja = caja;
-	}
-	public double getCantidad() {
-		return cantidad;
-	}
-	public void setCantidad(double cantidad) {
-		this.cantidad = cantidad;
-	}
 	
 	
 	
